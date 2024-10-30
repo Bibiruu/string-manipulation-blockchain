a7a92fd8ca6d9332b696550246b4ac8daeaacba5
Maria <so.mariagloria@gmail.com>
2024-10-30 07:52:29 +0200

    submit

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,14 @@
-module.exports = {
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*",
-    }
-  },
-  compilers: {
-    solc: {
-      version: "0.8.19",
-    }
-  },
+//import path from "path";
+
+export const networks = {
+  development: {
+    host: "127.0.0.1",
+    port: 7545,
+    network_id: "*",
+  }
+};
+export const compilers = {
+  solc: {
+    version: "0.8.19",
+  }
 };
