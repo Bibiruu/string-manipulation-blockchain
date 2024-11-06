378fdadecfbebf1092e41f5451e7d86879c51d19
Maria <so.mariagloria@gmail.com>
2024-11-06 20:19:13 +0200

    Remove nested Git repository and update .gitignore

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ The pure keyword means the function doesn't access or modify the blockchain's st
 The function length is pure because it only works with input values and doesn't interact with any contract variables stored on-chain.
 
 String manipulation should be used only for last resort in smart contracts. 
+
+1_initial_migration.js
+======================
+
+   Deploying 'Migrations'
+   ----------------------
+   > transaction hash:    0xcbdd51e24600a4a72e6757367cebaf0aedd73d62c58c464e4fa9e46ed11c97ca
+   
+2_strings_manipulation.js
+=========================
+
+   Deploying 'Strings'
+   -------------------
+   > transaction hash:    0x02cb370d09812fd99575a7255b3224f545bc99e745688bca4ed5c1445397e1ed
--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,40 @@
-//import path from "path";
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
 
-export const networks = {
-  development: {
-    host: "127.0.0.1",
-    port: 7545,
-    network_id: "*",
-  }
+// Exponential backoff formula: 2^attempt * 1000 (milliseconds)
+const exponentialBackoff = (attempt) => {
+  return Math.pow(2, attempt) * 1000;
 };
-export const compilers = {
-  solc: {
-    version: "0.8.19",
-  }
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+
+    sepolia: {
+      provider: () => new HDWalletProvider(
+        process.env.MNEMONIC,
+        `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHMY_ENDPOINT}`
+      ),
+      network_id: 11155111,
+      gas: 6000000,               // Gas limit*/
+      gasPrice: 20000000000,       // 20 Gwei
+      confirmations: 1,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      networkCheckTimeout: 10000,  // Increase if necessary
+      clientConfig: {
+        maxAttempts: 5,
+        interval: exponentialBackoff,
+      }
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.19",
+    }
+  },
 };
